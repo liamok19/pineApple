d2936091d884759ff136aed8b6786027e59e51fd
Liam O <lokanedeveloper@gmail.com>
2022-05-29 18:33:18 +1000

    imported model

--- pineapple/src/Pages/About.js
+++ pineapple/src/Pages/About.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Model from '../components/THREE'
 
 export default function About() {
     return (
       <div>
         <h2> About Us </h2>
+        < Model />
       </div>
     );
   }
